2ae3ada2250b48eaf14bd1c425c5e2e825c6ef16
Andrea <andrea30111@gmail.com>
2018-02-11 22:35:56 +0100

    registrazione in popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
   user: String;
+  registerMessage: String;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private authService: AuthService ,private alertCtrl: AlertController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -35,10 +36,74 @@ export class MyApp {
     }
   }
 
+
+  presentRegistration = function() {
+    let prompt = this.alertCtrl.create({
+      title: 'Registrazione',
+      message: "Benvenuto! Effettua la registrazione per usufruire di tutti i servizi!",
+      inputs: [
+        {
+          name: 'firstname',
+          placeholder: 'Insert your firstname'
+        },
+        {
+          name: 'lastname',
+          placeholder: 'Insert your lastname'
+        },
+        {
+          name: 'CF',
+          placeholder: 'Insert your fiscal code'
+        },
+        {
+          name: 'email',
+          placeholder: 'Insert your email'
+        },
+        {
+          name: 'password',
+          placeholder: 'Insert your password',
+          type: 'password'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Login',
+          handler: data => {
+           this.presentPrompt();
+          }
+        },
+        {
+          text: 'Register',
+          handler: data => {
+            const user = new User(
+              data.email, 
+              data.password, 
+              data.firstName, 
+              data.lastName, 
+              data.fiscalCode, 
+              null);
+              this.authService.signup(user)
+                  .subscribe(
+                      data => {
+                          console.log(data);
+                          this.registerMessage = data.message;
+                      },
+                      error => {
+                          console.error(error);
+                          this.registerMessage = error.title;
+                      }
+                  );
+          
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
   presentPrompt() {
     let prompt = this.alertCtrl.create({
       title: 'Login',
-      message: "Enter a name for this new album you're so keen on adding",
+      message: "Bentornato! Effettua il login e inizia a parcheggiare!",
       inputs: [
         {
           name: 'email',
@@ -52,9 +117,9 @@ export class MyApp {
       ],
       buttons: [
         {
-          text: 'Cancel',
+          text: 'Registrati',
           handler: data => {
-            console.log('Cancel clicked');
+            this.presentRegistration();
           }
         },
         {
@@ -87,5 +152,6 @@ export class MyApp {
     });
     prompt.present();
   }
+  
 }
 
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -7,11 +7,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
-  tab2 = 'HomeComponent';
+  tab1 = 'HomepagePage';
+  tab6 = 'TutorialPage';
   tab3 = 'MapsComponent';
   tab4 = 'MyparkingComponent';
   tab5 = 'ExplorePage';
-  tab6 = 'TutorialPage';
- 
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) { }
 }
